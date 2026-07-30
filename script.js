37b33321af975125b54721c8fa192ef69f5c4780
arthurnbarone-hash <arthurnbarone-hash@users.noreply.github.com>
2026-07-30 17:48:09 -0300

    Texto em duas linhas, Arthur Barone em bold #1a3c5c, menu e títulos na mesma cor

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 (function () {
-  const text = "Olá, eu sou Arthur Barone!";
+  const line1 = "Olá, eu sou";
+  const line2 = "Arthur Barone!";
   const el = document.getElementById("typewriter");
-  let i = 0;
+  let i = 0, j = 0;
+  let phase = 0;
 
   function type() {
-    if (i < text.length) {
-      el.innerHTML = text.slice(0, i + 1) + '<span class="cursor">|</span>';
-      i++;
-      setTimeout(type, 60);
-    } else {
-      el.innerHTML = text + '<span class="cursor">|</span>';
+    if (phase === 0) {
+      if (i < line1.length) {
+        el.innerHTML = '<span class="line-regular">' + line1.slice(0, i + 1) + '</span><br><span class="line-bold"></span><span class="cursor">|</span>';
+        i++;
+        setTimeout(type, 60);
+      } else {
+        phase = 1;
+        setTimeout(type, 300);
+      }
+    } else if (phase === 1) {
+      if (j < line2.length) {
+        el.innerHTML = '<span class="line-regular">' + line1 + '</span><br><span class="line-bold">' + line2.slice(0, j + 1) + '</span><span class="cursor">|</span>';
+        j++;
+        setTimeout(type, 60);
+      } else {
+        el.innerHTML = '<span class="line-regular">' + line1 + '</span><br><span class="line-bold">' + line2 + '</span><span class="cursor">|</span>';
+      }
     }
   }
 
